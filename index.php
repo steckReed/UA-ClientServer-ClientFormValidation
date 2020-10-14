38fd23ad6a21378edd3c22be58f57e3c87b09f28
steckReed <61429429+steckReed@users.noreply.github.com>
2020-10-14 19:37:40 -0400

    change for style and form layout

--- index.php
+++ index.php
@@ -2,7 +2,43 @@
   include './View/header.php';
 ?>
 
-<h1>Eh, put a form here or whatever</h1>
+<h1 class='welcome-message-text center'>Event Listing</h1>
+<div class='form-container box-shadow'>
+  <form>
+    <!-- Location & Title -->
+      <div class="form-row">
+          <div class="col"> 
+            <label for="eventLocation">Location</label>
+            <input id="eventLocation" type="text" class="form-control" placeholder="">
+          </div>  
+          <div class="col"> 
+            <label for="eventTitle">Title</label>
+            <input id="eventTitle" type="text" class="form-control" placeholder="">
+          </div>
+      </div>
+    <!-- Location & Title -->
+
+    <!-- Description -->
+            <label for="eventDescription">Description</label>
+            <textarea id="eventDescription" type="text" class="form-control" placeholder=""></textarea>
+    <!-- Description -->
+
+    <!-- Phone & Email -->
+      <div class="form-row">
+          <div class="col"> 
+            <label for="eventUserPhone">Phone Number</label>
+            <input id="eventUserPhone" type="tel" class="form-control" placeholder="555-555-5555">
+            <small>555-555-5555</small>
+          </div>
+          <div class="col"> 
+            <label for="eventUserEmail">Email address</label>
+            <input id="eventUserEmail" type="email" class="form-control" placeholder="Example@Example.com">
+          </div>
+      </div>
+    <!-- Phone & Email -->
+    <button type="submit" class="btn-event-submit btn btn-primary">Submit</button>
+  </form>
+</div>
 
 <?php
   include './View/header.php';
