56702fdd5042df516f977b487431485d7f7b6189
steckReed <61429429+steckReed@users.noreply.github.com>
2020-10-14 19:14:05 -0400

    basic setup for form validation (file structure)

---
+++ View/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Meta Tags -->
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Meta Tags -->
+
+    <!-- Links -->
+        <link rel='stylesheet' href="./View/Public/CSS/custom-styles.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- Links -->
+			
+
+    <!-- Scripts -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script> -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <!-- Scripts -->
+    
+    <title>Form Validation</title>
+</head>
+
+<body>
+    <div class='page-grid'>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+  include './View/header.php';
+?>
+
+<h1>Eh, put a form here or whatever</h1>
+
+<?php
+  include './View/header.php';
+?>
