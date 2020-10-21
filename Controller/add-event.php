214fc597633867b255700ffeca51b8701247c0e8
steckReed <61429429+steckReed@users.noreply.github.com>
2020-10-21 18:38:59 -0400

    final

--- Controller/add-event.php
+++ Controller/add-event.php
@@ -1,2 +1,37 @@
 <?php
+if (isset($_POST['form-add-event'])){    
 
+    $POST = filter_var_array($_POST, FILTER_SANITIZE_STRING);
+
+    // Clean and Extract Variables
+      $eventDate = trim(htmlentities($POST['eventDate']));
+      $eventTime = trim(htmlentities($POST['eventTime']));
+      $eventLocation = trim(htmlentities($POST['eventLocation']));
+      $eventTitle = trim(htmlentities($POST['eventTitle']));
+      $eventDescription = trim(htmlentities($POST['eventDescription']));
+      $eventUserPhone = trim(htmlentities($POST['eventUserPhone']));
+      $eventUserEmail = trim(htmlentities($POST['eventUserEmail']));
+    // Clean and Extract Variables
+    
+    // Set and display event information
+        echo"
+            <script>
+                // Set localStorage
+                    localStorage.setItem('eventDate', '{$eventDate}');
+                    localStorage.setItem('eventTime', '{$eventTime}');
+                    localStorage.setItem('eventLocation', '{$eventLocation}');
+                    localStorage.setItem('eventTitle', '{$eventTitle}');
+                    localStorage.setItem('eventDescription', '{$eventDescription}');
+                    localStorage.setItem('eventUserPhone', '{$eventUserPhone}');
+                    localStorage.setItem('eventUserEmail', '{$eventUserEmail}');
+                // Set localStorage
+
+                // Display localStorage
+                    var displayEventText = '<h3>Event Information Added:</h3><br><h5>Date: </h5>'+localStorage.getItem('eventDate')+'<br><br><h5>Event Time: </h5>'+localStorage.getItem('eventTime')+'<br><br><h5>Event Location: </h5>'+localStorage.getItem('eventLocation')+'<br><br><h5>Event Title: </h5>'+localStorage.getItem('eventTitle')+'<br><br><h5>Event Description: </h5>'+localStorage.getItem('eventDescription')+'<br><br><h5>Event Phone: </h5>'+localStorage.getItem('eventUserPhone')+'<br><br><h5>Event Email: </h5>'+localStorage.getItem('eventUserEmail');
+
+                    document.getElementById('localStorageData').innerHTML = displayEventText;
+                // Display localStorage
+            </script>
+        ";
+    // Set and display event information
+}
--- index.php
+++ index.php
@@ -6,17 +6,17 @@
 
 <div class='form-container box-shadow'>
   <!-- Form -->
-    <form>
+    <form action="" method="POST">
       <!-- Date & Time -->
         <div class="form-row">
           <div class="col"> 
             <label for="eventDate">Date</label>
-            <input id="eventDate" type="date" class="form-control" required>
+            <input id="eventDate" name='eventDate' type="date" class="form-control" required>
           </div>  
 
           <div class="col"> 
             <label for="eventTime">Time</label>
-            <input id="eventTime" type="time" class="form-control" required>
+            <input id="eventTime" name='eventTime' type="time" class="form-control" required>
           </div>
         </div>
       <!-- Date & Time -->
@@ -25,42 +25,45 @@
         <div class="form-row">
             <div class="col"> 
               <label for="eventLocation">Location</label>
-              <input id="eventLocation" type="text" class="form-control" placeholder="Polsky Room 222" minlength="10" maxlength="100" required>
+              <input id="eventLocation" name='eventLocation' type="text" class="form-control" placeholder="Polsky Room 222" minlength="10" maxlength="100" required>
             </div>
 
             <div class="col"> 
               <label for="eventTitle">Event Title</label>
-              <input id="eventTitle" type="text" class="form-control" placeholder="Hot Cocoa Party" minlength="1" maxlength="50" required>
+              <input id="eventTitle" name='eventTitle' type="text" class="form-control" placeholder="Hot Cocoa Party" minlength="1" maxlength="50" required>
             </div>
         </div>
       <!-- Location & Title -->
 
       <!-- Description -->
         <label for="eventDescription">Description</label>
-        <textarea id="eventDescription" type="text" class="form-control" placeholder="Enter Description Here" minlength="10" maxlength="200" required></textarea>
+        <textarea id="eventDescription" name='eventDescription' type="text" class="form-control" placeholder="Enter Description Here" minlength="10" maxlength="200" required></textarea>
       <!-- Description -->
 
       <!-- Phone & Email -->
         <div class="form-row">
           <div class="col"> 
             <label for="eventUserPhone">Phone Number</label>
-            <input id="eventUserPhone" type="tel" class="form-control" placeholder='555-867-5309' maxlength="12" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required>
+            <input id="eventUserPhone" name='eventUserPhone' type="tel" class="form-control" placeholder='555-867-5309' maxlength="12" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required>
             <small>555-867-5309</small>
           </div>
 
           <div class="col"> 
             <label for="eventUserEmail">Email address</label>
-            <input id="eventUserEmail" type="email" class="form-control" placeholder="email@Email.com" required>
+            <input id="eventUserEmail" name='eventUserEmail' type="email" class="form-control" placeholder="email@Email.com" required>
           </div>
         </div>
       <!-- Phone & Email -->
       
       <!-- Submit Button -->
-      <button type="submit" class="btn-event-submit btn btn-primary">Add Event</button>
+      <button name='form-add-event' type="submit" class="btn-event-submit btn btn-primary">Add Event</button>
     </form>
   <!-- Form -->
+
+  <div id="localStorageData" class='box-shadow'></div>
 </div>
 
 <?php
   include './View/footer.php';
+  include './Controller/add-event.php';
 ?>
