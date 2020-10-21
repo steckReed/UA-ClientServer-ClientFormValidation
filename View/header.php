6f80348345b85ace74598447f5936ad28673c356
steckReed <61429429+steckReed@users.noreply.github.com>
2020-10-20 21:31:25 -0400

    Form complete - Web storage API & display needed

--- View/header.php
+++ View/header.php
@@ -15,11 +15,10 @@
 
     <!-- Scripts -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script> -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <!-- Scripts -->
     
-    <title>Form Validation</title>
+    <title>Event Listing - Lab 03</title>
 </head>
 
 <body>
--- index.php
+++ index.php
@@ -3,43 +3,64 @@
 ?>
 
 <h1 class='welcome-message-text center'>Event Listing</h1>
+
 <div class='form-container box-shadow'>
-  <form>
-    <!-- Location & Title -->
-      <div class="form-row">
+  <!-- Form -->
+    <form>
+      <!-- Date & Time -->
+        <div class="form-row">
           <div class="col"> 
-            <label for="eventLocation">Location</label>
-            <input id="eventLocation" type="text" class="form-control" placeholder="">
+            <label for="eventDate">Date</label>
+            <input id="eventDate" type="date" class="form-control" required>
           </div>  
+
           <div class="col"> 
-            <label for="eventTitle">Title</label>
-            <input id="eventTitle" type="text" class="form-control" placeholder="">
+            <label for="eventTime">Time</label>
+            <input id="eventTime" type="time" class="form-control" required>
           </div>
-      </div>
-    <!-- Location & Title -->
+        </div>
+      <!-- Date & Time -->
 
-    <!-- Description -->
-            <label for="eventDescription">Description</label>
-            <textarea id="eventDescription" type="text" class="form-control" placeholder=""></textarea>
-    <!-- Description -->
+      <!-- Location & Title -->
+        <div class="form-row">
+            <div class="col"> 
+              <label for="eventLocation">Location</label>
+              <input id="eventLocation" type="text" class="form-control" placeholder="Polsky Room 222" minlength="10" maxlength="100" required>
+            </div>
 
-    <!-- Phone & Email -->
-      <div class="form-row">
+            <div class="col"> 
+              <label for="eventTitle">Event Title</label>
+              <input id="eventTitle" type="text" class="form-control" placeholder="Hot Cocoa Party" minlength="1" maxlength="50" required>
+            </div>
+        </div>
+      <!-- Location & Title -->
+
+      <!-- Description -->
+        <label for="eventDescription">Description</label>
+        <textarea id="eventDescription" type="text" class="form-control" placeholder="Enter Description Here" minlength="10" maxlength="200" required></textarea>
+      <!-- Description -->
+
+      <!-- Phone & Email -->
+        <div class="form-row">
           <div class="col"> 
             <label for="eventUserPhone">Phone Number</label>
-            <input id="eventUserPhone" type="tel" class="form-control" placeholder="555-555-5555">
-            <small>555-555-5555</small>
+            <input id="eventUserPhone" type="tel" class="form-control" placeholder='555-867-5309' maxlength="12" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required>
+            <small>555-867-5309</small>
           </div>
+
           <div class="col"> 
             <label for="eventUserEmail">Email address</label>
-            <input id="eventUserEmail" type="email" class="form-control" placeholder="Example@Example.com">
+            <input id="eventUserEmail" type="email" class="form-control" placeholder="email@Email.com" required>
           </div>
-      </div>
-    <!-- Phone & Email -->
-    <button type="submit" class="btn-event-submit btn btn-primary">Submit</button>
-  </form>
+        </div>
+      <!-- Phone & Email -->
+      
+      <!-- Submit Button -->
+      <button type="submit" class="btn-event-submit btn btn-primary">Add Event</button>
+    </form>
+  <!-- Form -->
 </div>
 
 <?php
-  include './View/header.php';
+  include './View/footer.php';
 ?>
